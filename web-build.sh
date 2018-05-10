6c2f9c1f26ad90367021fda2bd3e288802f5ce88
Tianxi Ku <txku@thoughtworks.com>
2018-05-10 10:58:28 +0800

    build and start website alongside with node backend

--- web-build.sh
+++ web-build.sh
@@ -4,7 +4,7 @@ if [ -d src/web ]
 then
     cd src/web && git checkout . && git pull --rebase
 else
-	cd src && git clone git@github.com:TisnKu/joke-scheduler-web.git web && cd web
+	cd src && git clone https://github.com/TisnKu/joke-scheduler-web.git web && cd web
 fi
 
-npm install
+npm install && ([ ! -d ../../dist/web ] || rm -rf ../../dist/web) && mkdir ../../dist/web && cp -r ./dist ../../dist/web/dist
