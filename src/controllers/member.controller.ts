b45026d109d8507b9b20dc7a7205c21d1c5af9a2
Tianxi Ku <txku@thoughtworks.com>
2018-05-13 01:47:04 +0800

    add http status code for apis

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,7 +1,18 @@
-import { BodyParams, Controller, Delete, Get, PathParams, Post, QueryParams, Required } from 'ts-express-decorators';
+import {
+    BodyParams,
+    Controller,
+    Delete,
+    Get,
+    PathParams,
+    Post,
+    QueryParams,
+    Required,
+    Status
+} from 'ts-express-decorators';
 import { MemberService } from '../services/member.service';
 import { Member } from '../entity/Member';
 import { DeleteResult } from 'typeorm';
+import { HTTPStatusCodes } from '../types/http';
 
 @Controller('/member')
 export class MemberController {
@@ -10,16 +21,19 @@ export class MemberController {
     }
 
     @Get('')
+    @Status(HTTPStatusCodes.OK)
     async getTeamMembers(@Required() @QueryParams('team_id') teamId: number): Promise<Member[]> {
         return await this.memberService.getAllMembersByTeamId(teamId);
     }
 
     @Post('')
+    @Status(HTTPStatusCodes.CREATED)
     async createMembers(@BodyParams() body: Member[]) {
         return await this.memberService.createMembers(body);
     }
 
     @Delete('/:id')
+    @Status(HTTPStatusCodes.OK)
     async deleteMember(@PathParams('id') memberId: number): Promise<DeleteResult> {
         return await this.memberService.deleteMember(memberId);
     }
--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Patch, QueryParams, Required } from 'ts-express-decorators';
+import { Controller, Get, Patch, QueryParams, Required, Status } from 'ts-express-decorators';
 import { ScheduleService } from '../services/schedule.service';
 import { Schedule } from '../entity/Schedule';
 import { TodayScheduleDTO } from '../dto/TodayScheduleDTO';
+import { HTTPStatusCodes } from '../types/http';
 
 @Controller('/schedule')
 export class ScheduleController {
@@ -10,11 +11,13 @@ export class ScheduleController {
     }
 
     @Get('/today')
+    @Status(HTTPStatusCodes.OK)
     async getUser(@Required() @QueryParams('team_id') teamId: number): Promise<TodayScheduleDTO> {
         return this.scheduleService.getTodayScheduleResponse(teamId);
     }
 
     @Patch('/move')
+    @Status(HTTPStatusCodes.OK)
     async moveTodaySchedule(@QueryParams('team_id') teamId: number, @QueryParams('is_forward') isForward: boolean): Promise<Schedule> {
         return this.scheduleService.moveTodaySchedule(teamId, isForward);
     }
